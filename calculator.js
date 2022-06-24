83df6dcaff2f20ed117a2bd9813f98dd90920b6e
Vianey <vianeynkem@gmail.com>
2022-06-24 03:24:38 +0100

    calculator

---
+++ calculator.js
@@ -0,0 +1,12 @@
+a = parseInt(prompt("Enter first number"));
+b = parseInt(prompt("Enter second number"));
+
+let x = a + b;
+let y = a - b;
+let z = a * b;
+let u = a / b;
+
+alert("Sum of " + a + " and " + b + " is : " + x );
+alert("Difference of " + a + " and " + b + " is : " + y );
+alert("Product of " + a + " and " + b + " is : " + z );
+alert("Quoitent of " + a + " and " + b + " is : " + u );
