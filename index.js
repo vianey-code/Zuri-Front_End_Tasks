cb6e3af48ac6795cfa604836f794a0702a6db2f4
Ezebilo Nkemjika <66277432+vianey-code@users.noreply.github.com>
2022-06-28 10:17:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-<script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Information</title>
+</head>
+<script type="text/javascript" scr="index.js">
     document.write("<H1>Name: Ezebilo Nkemjika Vianey")
     document.write("<H1>Height: 5ft")
     document.write("<H1>Country: Nigeria")
-</script> 
+</script>
+<body>
+    
+</body>
+</html>
